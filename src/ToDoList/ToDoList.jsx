65fbc6a6fdc4874c40d9abab7dd705f9725666e1
Alexander Titov <titovtrio@gmail.com>
2024-12-08 18:20:10 +0300

    project init

---
+++ src/ToDoList/ToDoList.jsx
@@ -0,0 +1,5 @@
+import { ToDoListLayout } from './ToDoListLayout';
+
+export const ToDoList = () => {
+	return <ToDoListLayout />;
+};
---
+++ src/ToDoList/ToDoListLayout.jsx
@@ -0,0 +1,6 @@
+export const ToDoListLayout = () => (
+	<>
+		<div>Список дел:</div>
+		<ul></ul>
+	</>
+);
